767abbbbcfff4f27bb9ad7285b0d0dabe5443e77
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-08-17 09:22:32 +0000

    Add basic quiz functionality

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to my python quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() == "yes" or playing == "y":
+    print("Okay! Let's play :)")
+else:
+    quit()
+
+score = 0
+
+answer = input("What does CPU stand for? ")
+
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect :(")
+
+answer = input("What does GPU stand for? ")
+
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect :(")
+
+answer = input("What does RAM stand for? ")
+
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect :(")
+
+answer = input("What does PSU stand for? ")
+
+if answer.lower() == "power supply unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect :(")
+
+print(f"Congratulations! Your score is: {score}")   
+print("You got " + str(round((score/4) * 100)) + "%.")
